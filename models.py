4f23eaeb81c046dfae0a16048ac82d29ebef4bca
@levchenko <levchenko@welltory.app>
2024-10-20 03:01:46 +0200

    Alembic init

--- models.py
+++ models.py
@@ -1,11 +1,20 @@
-from typing import Optional
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    internal_user_id = Column(String, index=True)
+    hashed_sub = Column(String, unique=True, index=True)
 
 
-class GithubUserModel(BaseModel):
-    name: Optional[str]
-    blog: str
-    bio: Optional[str]
-    public_repos: int
-    followers: int
-    avatar_url: str
+class TaskSolution(Base):
+    __tablename__ = 'tasksolution'
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    lesson_name = Column(String, nullable=False)
+    completed_at = Column(DateTime, default=func.now(), nullable=False)
