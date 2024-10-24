2a73ee1714aee75ef30f23a62f56ab9e44a82675
@levchenko <levchenko@welltory.app>
2024-10-24 03:51:53 +0200

    Activate summary

--- routes/lesson.py
+++ routes/lesson.py
@@ -74,6 +74,7 @@ def get_lesson_data(
                     "points": 0,
                     "order": 0,
                     "isSolved": False,
+                    "is_active": True,
                 }
 
                 # Set prevUrl for summary
